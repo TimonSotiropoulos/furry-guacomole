6a22acf832be1d606df653419f11b45f8030ea17
Timon Sotiropoulos <timon@seedtech.io>
2016-05-04 20:40:40 +1000

    Added normalise script and created animation buttons

---
+++ src/app/components/Button.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class Button extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.styles = {
+            buttonCont: {
+                width: '200px',
+                height: '20px',
+                padding: '20px 50px',
+                backgroundColor: '#999999',
+                color: 'white',
+                alignText: 'center',
+            },
+        };
+    }
+
+    handleClick() {
+        console.log("Button Clicked!");
+
+    }
+
+    render() {
+        return (
+            <div style={this.styles.buttonCont} onClick={this.handleClick}>
+                {this.props.buttonText}
+            </div>
+        );
+    }
+}
--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
 
-import Body from './Body.jsx';
+import Body from './Body';
+import Button from './Button';
 
 export default class Main extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.styles = {
+            main: {
+                width: '100%',
+                height: '100vh',
+                'backgroundColor': '#000'
+            },
+            
+        }
+
+    }
+
     render() {
-        console.log(this.props.name);
+
         return (
-            <div>
-                Hello World, Some new content! OMG!
-                <Body />
+            <div style={this.styles.main}>
+                <div style={}></div>
+                <Button buttonText='Pause Animation'/>
+                <Button buttonText='Start Animation'/>
+
+
             </div>
         );
     }
